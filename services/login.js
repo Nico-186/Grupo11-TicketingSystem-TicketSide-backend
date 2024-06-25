5a7fc407ad78abcc69138ac60d969f16bf1d43d9
Nico-186 <rnicolastabilo@gmail.com>
2024-06-24 22:55:46 -0400

    login fix

--- services/login.js
+++ services/login.js
@@ -4,7 +4,7 @@ const helper = require('../helper');
 async function get(username, password) {
   const conn = await db.connection();
   const rows = await conn.execute(
-    `SELECT ID_usuario, Contraseña, nomusua, rol FROM grupo11.usuario WHERE nomusua='${username}' AND Contraseña='${password}';`
+    `SELECT * FROM grupo11.usuario WHERE nomusua='${username}' AND Contraseña='${password}';`
   );
   const data = helper.emptyOrRows(rows);
   conn.release();
